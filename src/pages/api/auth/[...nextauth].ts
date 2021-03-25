52cc299b4c4a832c947a9fbf7f1e5341fbd083c9
gyuu-neves <gyupro5@gmail.com>
2021-03-25 18:19:18 -0300

    added services

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { fauna } from '../../../services/faunadb'
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -9,4 +12,42 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+
+  // jwt: { 
+  //   signingKey: process.env.SIGNIN_KEY, 
+  // },
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('user'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
